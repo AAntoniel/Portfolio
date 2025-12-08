eb6e12970df4727e1f23ed046da8059586a592ce
Antoniel <antonielstefaniak@gmail.com>
2025-12-08 20:12:22 -0300

    Fist portfolio commit

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("👨‍💻 Portfólio de Antoniel")
+
+st.write(
+    """
+    Bem-vindo ao meu portfólio!  
+    Escolha um dos projetos no menu à esquerda.
+    """
+)

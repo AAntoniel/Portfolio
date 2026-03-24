c43bae4454dd0a9263cb5e9bd2de8c4ead5ca5cc
Antoniel K. Stefaniak <antonielstefaniak@gmail.com>
2026-03-24 15:42:11 -0300

    feat: changing ai agent architecture (local->decoupled)

--- pages/4_Resume_Chatbot.py
+++ pages/4_Resume_Chatbot.py
@@ -1,37 +1,50 @@
 import streamlit as st
 from chatbot.chatbot import agent_response
+import requests
 
 st.set_page_config(page_title="Antoniel AI - Portfólio")
 st.title("🤖 Converse com o meu Currículo")
 st.markdown(
     "Olá! Eu sou o assistente virtual do **Antoniel**. Pergunte-me sobre ele, habilidades, experiências ou formação!")
 
+url = "https://resume-ai-agent-2-oinz.onrender.com/ask"
+
 # (Session State)
 # Se for a primeira vez que o usuário abre a página, criamos uma lista de mensagens
-if "mensagens" not in st.session_state:
-    st.session_state.mensagens = [
+if "messages" not in st.session_state:
+    st.session_state.messages = [
         {"role": "assistant", "content": "Como posso te ajudar a conhecer melhor o perfil do Antoniel?"}
     ]
 
 # Deixa na tela todas as mensagens que já estão salvas na memória
-for msg in st.session_state.mensagens:
+for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
 # A Caixa de Texto (Input) e a Lógica de Resposta
-pergunta = st.chat_input("Ex: Onde ele estudou?")
+question = st.chat_input("Ex: Onde ele estudou?")
 
-if pergunta:
+if question:
     # Mostra a pergunta do usuário na tela e salva na memória
     with st.chat_message("user"):
-        st.markdown(pergunta)
-    st.session_state.mensagens.append({"role": "user", "content": pergunta})
+        st.markdown(question)
+    st.session_state.messages.append({"role": "user", "content": question})
 
-    # Chama a função agent_response
+    user_input_dict = {
+        "text": question
+    }
+
     with st.chat_message("assistant"):
         with st.spinner("Analisando o currículo..."):
-            response = agent_response(pergunta)
-            st.markdown(response)
+            try:
+                response = requests.post(url, json=user_input_dict)
+                response.raise_for_status()
 
-    # Salva a resposta da IA na memória para não sumir no próximo recarregamento
-    st.session_state.mensagens.append({"role": "assistant", "content": response})
+                result = response.json()
+                final_response = result["resposta"]
+
+                st.markdown(final_response)
+
+                st.session_state.messages.append({"role": "assistant", "content": final_response})
+            except requests.exceptions.RequestException as e:
+                st.error(f"Error connecting to the API. {e}")
